5bf8bb3b39cddb82ce9d681bfbac055f25311428
James Villanueva <jameslawrence.rivera.villanueva@gmail.com>
2022-03-11 15:20:01 +0800

    added a delete function for each task and button delete

--- src/components/List/TodoList.js
+++ src/components/List/TodoList.js
@@ -3,26 +3,55 @@ import {Fragment, useEffect, useState, useContext} from 'react';
 import Container from '../UI/Container/Container';
 import AuthContext from '../../task/auth-context';
 import Header from '../Layout/Header';
+import {AiOutlineDelete} from 'react-icons/ai';
 
 const TodoList = props => {
 	// props values... 
 	const { ListItems, onClickItem } = props; 
 	// task context... 
 	const taskCtx = useContext(AuthContext);
+	const {onChange, item, clicked, listTask, onDelete, onReset, setItem} = taskCtx;
 
-	const Items = ListItems.map((item, index) => {
-		return <div key={index} className='list-wrapper border-list'>
-			<p>{item.taskName}</p>
-			<p className='sub-detail'>{item.detail}</p>
-			{item.isDone 
-				? <p className='sub-complete'>Complete</p>
-				: <p className='sub-in-progress'>In-progress</p>}
+	let [btnClick, setBtnClick] = useState(false);
+	const [taskList, setTaskList] = useState([]);
+	const clickHandler = e => {
+		let item = e.target.value;
+		onClickItem(item, e);
+	};
 
-		</div>
-	});
+	const onDeleteHandler = e => {
+		const id= parseInt(e.target.value);
+		onDelete(id);
+		setBtnClick(!btnClick);
+	};
+
+	useEffect(() => {
+		const Items = ListItems.map((item, index) => {
+			return <div key={index} className='list-wrapper border-list'>
+				<div>
+					<p>{item.taskName}</p>
+					<p className='sub-detail'>{item.detail}</p>
+					{item.isDone 
+						? <p className='sub-complete'>Complete</p>
+						: <p className='sub-in-progress'>In-progress</p>}
+				</div>
+				<button 
+						type='button' 
+						className='btn-delete' 
+						value={index}
+						onClick={onDeleteHandler}><AiOutlineDelete className='icon-delete'/></button>	
+
+			</div>
+		});
+		setTaskList(Items)
+	}, [listTask,btnClick])
 	return <Container>
 		<Header/>
-		{Items}		
+		{taskList.length > 0 
+				? <div className='list-container'>{taskList}</div>
+				: <div className='list-container'>
+					<p> You're all good. Your task is empty! </p>
+				</div>}	
 	</Container>
 }
 
